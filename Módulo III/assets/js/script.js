38eeb2e2eb60603a727d4e5a9f247e005493303b
Jatabara <patrickluizjf@hotmail.com>
2022-06-19 21:11:14 -0300

    Add

---
+++ Módulo III/assets/js/script.js
@@ -0,0 +1,8 @@
+// Introdução ao Javascript //
+console.log("Hello, World");
+// DOM //
+
+function color() {
+    var heading1 = document.getElementsByTagName('h1')[0]
+    heading1.style.color = 'red'
+}
