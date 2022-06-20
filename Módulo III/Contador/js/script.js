6672c453fc2934230efd91d3ae7a4630672bd7b5
Jatabara <patrickluizjf@hotmail.com>
2022-06-19 22:13:13 -0300

    Contador Modulo III

---
+++ Módulo III/Contador/js/script.js
@@ -0,0 +1,17 @@
+// Introdução ao Javascript //
+console.log("Hello, World");
+// DOM //
+
+var currentNumberWrapper = document.getElementById('currentNumber');
+
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
