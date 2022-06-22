dd7dd53d613e92f1c3f54380e5fecbb4cd9a88af
Jatabara <patrickluizjf@hotmail.com>
2022-06-22 18:00:36 -0300

    Proj Alunos init

---
+++ Módulo III/Aprovação/js/script.js
@@ -0,0 +1,24 @@
+console.log("Hello, World");
+
+
+let alunoReg = []
+
+let media
+
+let aprovados = []
+
+
+
+function registraAluno() {
+
+}
+
+
+function alunoAprovados(arr, media) {
+
+    for (let i = 0; < arr.length; i++) {
+
+        if ()
+    }
+
+}
